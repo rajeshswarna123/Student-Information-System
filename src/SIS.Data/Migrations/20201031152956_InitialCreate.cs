7d6b008b263f606cb9c09f92f500d1cec5a8e8e4
Rajesh Swarna <rajeshswarna123@gmail.com>
2020-10-31 21:03:03 +0530

    Added Migrations

---
+++ src/SIS.Data/Migrations/20201031152956_InitialCreate.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SIS.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "SIS");
+
+            migrationBuilder.CreateTable(
+                name: "ClassCurriculum",
+                schema: "SIS",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClassId = table.Column<long>(nullable: false),
+                    SubjectId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassCurriculum", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                schema: "SIS",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marks",
+                schema: "SIS",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubjectId = table.Column<long>(nullable: false),
+                    StudentId = table.Column<long>(nullable: false),
+                    MaxMarks = table.Column<int>(nullable: false),
+                    ObtainedMarks = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                schema: "SIS",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    RollNumber = table.Column<int>(nullable: false),
+                    ClassId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                schema: "SIS",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "SIS",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                schema: "SIS",
+                table: "Users",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClassCurriculum",
+                schema: "SIS");
+
+            migrationBuilder.DropTable(
+                name: "Classes",
+                schema: "SIS");
+
+            migrationBuilder.DropTable(
+                name: "Marks",
+                schema: "SIS");
+
+            migrationBuilder.DropTable(
+                name: "Students",
+                schema: "SIS");
+
+            migrationBuilder.DropTable(
+                name: "Subjects",
+                schema: "SIS");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "SIS");
+        }
+    }
+}
