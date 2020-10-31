fea9f0470dd43009ff636602e0ce050f87134618
Rajesh Swarna <rajeshswarna123@gmail.com>
2020-10-31 20:56:20 +0530

    Added Navigation

--- src/SIS.Client/Program.cs
+++ src/SIS.Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.LocalStorage;
 
 namespace SIS.Client
 {
@@ -19,7 +20,10 @@ namespace SIS.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddBlazoredLocalStorage();
+
             await builder.Build().RunAsync();
+
         }
     }
 }
---
+++ src/SIS.Client/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿//using System.Net.Http;
+//using System.Net.Http.Headers;
+//using System.Threading.Tasks;
+
+//namespace SIS.Client
+//{
+//    public static class ServiceExtensions
+//    {
+//        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url, AuthenticationHeaderValue authorization)
+//        {
+//            var request = new HttpRequestMessage(HttpMethod.Get, url);
+//            request.Headers.Authorization = authorization;
+
+//            var response = await httpClient.SendAsync(request);
+//            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+//            return JsonSerializer.Parse<T>(responseBytes, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+//        }
+//    }
+//}
